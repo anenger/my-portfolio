92f20de9a206a8b3e2de93b7907c95fb584a1e0f
Andrew Enger <andrew.enger48@gmail.com>
2022-12-12 13:57:42 -0800

    Remove react helmet

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,11 @@
 import * as React from "react";
 import NotFound from "../components/notFound";
+import SeoItem from "../components/seo";
 
 const NotFoundPage = () => {
   return <NotFound></NotFound>;
 };
 
-export const Head = () => <title>Page Not Found</title>;
+export const Head = () => <SeoItem title={"Page Not Found"} />;
 
 export default NotFoundPage;
