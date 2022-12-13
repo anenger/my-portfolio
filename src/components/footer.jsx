95bcb4af4bc0314e40e132d26122bd7a752d6b46
Andrew Enger <andrew.enger48@gmail.com>
2022-12-13 15:55:57 -0800

    More css updates yeehaw

---
+++ src/components/footer.jsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+import { footer, footerDiv, footerLink, footerIcon } from "./footer.module.css";
+import { hoverUnderlineAnimation } from "./global.module.css";
+import { GrGatsbyjs } from "react-icons/gr";
+
+const Footer = () => {
+  return (
+    <footer className={footer}>
+      <div className={footerDiv}>
+        <a
+          href="https://github.com/anenger/my-portfolio"
+          className={`${footerLink} ${hoverUnderlineAnimation}`}
+        >
+          This site was built with Gatsby.js
+        </a>
+        <GrGatsbyjs className={footerIcon} />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import Navbar from "./navbar";
+import Footer from "./footer";
 import { parentContainer, childrenContainer } from "./layout.module.css";
 
 const Layout = ({ children }) => {
@@ -8,6 +9,7 @@ const Layout = ({ children }) => {
     <div className={parentContainer}>
       <Navbar />
       <main className={childrenContainer}>{children}</main>
+      <Footer />
     </div>
   );
 };
