ee624c88882dacfdbc97b8f8fcce92ea8fceeee7
Andrew Enger <andrew.enger48@gmail.com>
2022-12-19 18:25:18 -0500

    css filename

--- src/components/resumeItem.jsx
+++ src/components/resumeItem.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { hoverUnderlineAnimation } from "../components/global.module.css";
+import { hoverUnderlineAnimation } from "./global.module.css";
 
 import {
   resumeJobItem,
