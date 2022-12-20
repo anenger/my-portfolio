65eeaa7142818f4d2de89f8ded7a0aca5dbbba15
Andrew Enger <andrew.enger48@gmail.com>
2022-12-20 00:14:59 -0500

    ganalytics again

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,27 @@ module.exports = {
     image: `/icon.png`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ["G-9Y7BMCXF75"],
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
