6b4625da9fb71810035f087a46003719f4b055eb
Andrew Enger <andrew.enger48@gmail.com>
2022-12-21 18:23:09 -0500

    fix weird 404 behavior

--- src/components/notFound.jsx
+++ src/components/notFound.jsx
@@ -1,23 +1,33 @@
 import * as React from "react";
 import { Link } from "gatsby";
 
-import { page, heading, paragraph, linkText } from "./notFound.module.css";
+import {
+  notFoundDiv,
+  notFoundHeading,
+  notFoundParagraph,
+  notFoundLinkText,
+} from "./notFound.module.css";
 import { hoverUnderlineAnimation } from "./global.module.css";
 
 const NotFoundPage = () => {
   return (
     <main>
-      <body className={page}>
-        <span className={heading}>Page not found</span>
-        <p className={paragraph}>
-          Sorry 😔, we couldn't find what you were looking for.
-          <br />
-          <br />
-          <Link to="/" className={`${linkText} ${hoverUnderlineAnimation}`}>
-            Go home
-          </Link>
-          .
-        </p>
+      <body>
+        <div className={notFoundDiv}>
+          <span className={notFoundHeading}>Page not found</span>
+          <p className={notFoundParagraph}>
+            Sorry 😔, we couldn't find what you were looking for.
+            <br />
+            <br />
+            <Link
+              to="/"
+              className={`${notFoundLinkText} ${hoverUnderlineAnimation}`}
+            >
+              Go home
+            </Link>
+            .
+          </p>
+        </div>
       </body>
     </main>
   );
